340281135186cf22a3114b3fea8071577eb3966b
giannisbalatsoukas <johnbalatsoukas03@gmail.com>
2026-02-18 11:41:50 +0200

    Update arc length parameter in forward kinematics
    Adjusted the arc length parameter 's' from 0.048 to 0.05.

--- forward_kinematics.m
+++ forward_kinematics.m
@@ -9,9 +9,10 @@ function [px, py, pz] = forward_kinematics(theta1, k, d)
     % cosd(90) is exactly zero
     % cos(pi/2) reflects the accuracy of the floating point value of pi 
 
-    s = 0.048; 
+    s = 0.05; 
     
     px = -(1/k)*cosd(theta1)*(1-cos(s*k));
     py = -(1/k)*sind(theta1)*(1-cos(s*k));
     pz = d + (1/k)*sin(s*k)
+
 end
